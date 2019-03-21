dbbd237a780e276b8224eef28a67ec68e0f0e368
Vladimir Gourov <vladimir.gourov@blackrock.com>
2019-03-21 02:27:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 function theFunk(){
-  return "FUNKY!"
+  return theFunk
 }
